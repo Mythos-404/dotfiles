3cdf6f43a2576774a927c0945c2dc9b3f4c76c5b
Mythos_404 <mythos_404@outlook.com>
2024-05-06 21:25:49 +0800

    feat(yazi): clear code

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -62,7 +62,7 @@ end
 
 function Header:host()
 	if ya.target_family() ~= "unix" then
-		return ui.Line {}
+		return ui.Line({})
 	end
 	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
 end
@@ -70,8 +70,8 @@ end
 function Header:render(area)
 	self.area = area
 
-	local right = ui.Line { self:count(), self:tabs() }
-    local left = ui.Line { self:host(), self:cwd(math.max(0, area.w - right:width())) }
+	local right = ui.Line({ self:count(), self:tabs() })
+	local left = ui.Line({ self:host(), self:cwd(math.max(0, area.w - right:width())) })
 	return {
 		ui.Paragraph(area, { left }),
 		ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
