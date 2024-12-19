41511afbc179847885ff5332738d90dabe8b7f42
Mythos_404 <mythos_404@outlook.com>
2024-12-20 00:37:22 +0800

    fix(ipython): `00-import` not global var

--- .ipython/profile_default/startup/00-import.py
+++ .ipython/profile_default/startup/00-import.py
@@ -12,17 +12,19 @@ import time
 from functools import *
 from itertools import *
 
-modules = {
-    "tq": ["tqdm"],
-    "fn": ["funcy"],
-    "mi": ["more_itertools"],
-    "rtq": ["tqdm.rich"],
-    "json": ["orjson"],
-}
+def _():
+    modules = {
+        "tq": ["tqdm"],
+        "fn": ["funcy"],
+        "mi": ["more_itertools"],
+        "rtq": ["tqdm.rich"],
+        "json": ["orjson"],
+    }
 
-for alias, module in modules.items():
-    if importlib.util.find_spec(module[0]) is not None:
-        if len(module) == 1:
-            globals().update({alias: importlib.import_module(module[0])})
-        else:
-            globals().update({alias: getattr(importlib.import_module(module[0]), module[1])})
+    for alias, module in modules.items():
+        if importlib.util.find_spec(module[0]) is not None:
+            if len(module) == 1:
+                globals().update({alias: importlib.import_module(module[0])})
+            else:
+                globals().update({alias: getattr(importlib.import_module(module[0]), module[1])})
+_()
